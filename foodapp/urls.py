5a2db1e013476904f5372cf5c337a4b67b37e1ad
koushikromel <koushikromel@gmail.com>
2023-03-09 17:17:49 +0530

    added static files when debug false

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-         path('',views.index,name='index'),
-         path('menus',views.menus,name='menus'),
-         path('plans',views.plans,name='plans'),
-         path('login',views.login,name='login'), 
-         path('cart',views.cart,name='cart'), 
-         path('undercooking',views.undercooking,name='undercooking')
+  path('',views.index,name='index'),
+  path('menus',views.menus,name='menus'),
+  path('plans',views.plans,name='plans'),
+  path('login',views.login,name='login'), 
+  path('cart',views.cart,name='cart'), 
+  path('undercooking',views.undercooking,name='undercooking'),
 ]
--- foodproject/settings.py
+++ foodproject/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -106,10 +108,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/static/images/'
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
