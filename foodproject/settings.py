78de97580302a4fee6e35fe670dbc606662da4c6
koushikromel <koushikromel@gmail.com>
2023-03-09 17:19:05 +0530

    settings change

--- foodproject/settings.py
+++ foodproject/settings.py
@@ -110,14 +110,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/static/images/'
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 
 
